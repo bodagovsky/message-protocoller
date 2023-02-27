0cff0910994ba23d3dd20564507e5d7596610dde
Artur Bodagovskiy <aabodagovskiy@avito.ru>
2023-02-27 17:25:37 +0400

    hide secrets

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from telethon import TelegramClient, events
 from kafka_client import AIOProducer
 
-api_id = 29016752
-api_hash = 'c157aba1d9ca2aefbc34e51118c040d6'
+with open("api_id") as api_id_file:
+    api_id = api_id_file.readline()
+
+with open("api_hash") as api_hash_file:
+    api_hash = api_hash_file.readline()
+
+
 client = TelegramClient('anon', api_id, api_hash)
 
 kafka_producer = AIOProducer()
 
 @client.on(events.NewMessage(chats=[844182202]))
 async def new_message_handle(message):
-    # print(message)
     await kafka_producer.produce("chan-msg", message.message.message)
 
 def run():
